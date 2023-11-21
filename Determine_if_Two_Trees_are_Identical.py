ac7bdd40bbe846b5e9d1a9ed1286253f7ce9237f
DEEPRA MAZUMDER <deepra.mazumder.aiml25@heritageit.edu.in>
2023-11-21 23:31:00 +0530

    21st November 2023

---
+++ Determine_if_Two_Trees_are_Identical.py
@@ -0,0 +1,48 @@
+'''
+Given two binary trees, the task is to find if both of them are identical or not.
+Note: You need to return true or false, the printing is done by the driver code.
+
+Example 1:
+
+Input:
+     1          1
+   /   \      /   \
+  2     3    2     3
+  
+Output: 
+Yes
+
+Explanation: 
+There are two trees both having 3 nodes and 2 edges, both trees are identical having the root as 1, left child of 1 is 2 and right child of 1 is 3.
+
+Example 2:
+
+Input:
+    1       1
+  /  \     /  \
+ 2    3   3    2
+ 
+Output: 
+No
+
+Explanation: There are two trees both having 3 nodes and 2 edges, but both trees are not identical.
+
+Your task:
+Since this is a functional problem you don't have to worry about input, you just have to complete the function isIdentical() that takes two roots as parameters and returns true or false.
+The printing is done by the driver code.
+
+Expected Time Complexity: O(N).
+Expected Auxiliary Space: O(Height of the Tree).
+
+Constraints:
+1 <= Number of nodes <= 105
+1 <=Data of a node <= 109
+'''
+
+class Solution:
+    def isIdentical(self,root1, root2):
+        if root1 == None and root2 == None: return True
+        elif root1 == None or root2 == None: return False
+        if root1.data == root2.data:
+            return (self.isIdentical(root1.left , root2.left) and self.isIdentical(root1.right , root2.right))
+        return False
