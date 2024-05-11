e4d6147d126bb67510612eca19a1d4753e833461
DEEPRA MAZUMDER <deepra.mazumder.aiml25@heritageit.edu.in>
2024-05-11 11:56:49 +0530

    11th May 2024

--- Juggler_Sequence.cpp
+++ Juggler_Sequence.cpp
@@ -34,16 +34,16 @@ Constraints:
 class Solution 
 {
     public:
-    vector<long long> jugglerSequence(long long n) 
-    {
-        vector<long long> ans;
-        ans.push_back(n);
-        while (n != 1) 
+        vector<long long> jugglerSequence(long long n) 
         {
-            if (n % 2 == 0) n = sqrt(n);
-            else n = sqrt(n) * sqrt(n) * sqrt(n);
+            vector<long long> ans;
             ans.push_back(n);
+            while (n != 1) 
+            {
+                if (n % 2 == 0) n = sqrt(n);
+                else n = sqrt(n) * sqrt(n) * sqrt(n);
+                ans.push_back(n);
+            }
+            return ans;
         }
-        return ans;
-    }
 };
