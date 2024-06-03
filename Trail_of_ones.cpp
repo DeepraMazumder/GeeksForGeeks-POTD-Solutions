2de141ea8885b57fb58e50b2c4fecf9e37185ea1
DEEPRA MAZUMDER <deepra.mazumder.aiml25@heritageit.edu.in>
2024-06-03 10:17:53 +0530

    3rd June 2024

--- Trail_of_ones.cpp
+++ Trail_of_ones.cpp
@@ -1,5 +1,5 @@
 /*
-Given a number n, find the number of binary strings of length n that contain consecutive 1's in them. Since the number can be very large, return the answer after modulo with 1e9+7.
+Given a number n, find the number of binary strings of length n that contain consecutive 1's in them. Since, the number can be very large, return the answer after modulo with 1e9+7.
 
 Example 1:
 
